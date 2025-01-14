74dd6fa9c42ffd816fa1220ee9fb4e085b3582f6
Matthew Pendleton <MatthewAustinPendleton@gmail.com>
2025-01-14 16:26:33 -0500

    Add 16x16 grid creation with hover effects

---
+++ script.js
@@ -0,0 +1,18 @@
+const gridContainer = document.querySelector(".grid-container");
+
+for (let i = 0; i < 16; i++) {
+    let innerDiv = document.createElement("div");
+    innerDiv.classList.add("inner-div");
+    for (let j = 0; j < 16; j++) {
+        let gridCell = document.createElement("div");
+        gridCell.classList.add("grid-cell");
+        innerDiv.appendChild(gridCell);
+    }
+    gridContainer.appendChild(innerDiv);
+}
+
+gridContainer.addEventListener("mouseover", (e) => {
+    if (e.target.classList.contains("grid-cell")) {
+        e.target.style.backgroundColor = "black";//"rgb(242, 224, 224)"; 
+    }
+});
